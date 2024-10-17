b7b0990b896a5a678776e1c00bddc8e9d63b548c
HANDZCZ <handz@email.cz>
2024-10-17 20:09:51 +0200

    Add email validation to RegisterData

--- src/models/register_data.rs
+++ src/models/register_data.rs
@@ -1,7 +1,10 @@
+use garde::Validate;
 use serde::Deserialize;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Validate)]
 pub struct RegisterData {
+    #[garde(email)]
     pub email: String,
+    #[garde(skip)]
     pub password: String,
 }
