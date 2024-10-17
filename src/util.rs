ed9257c0cb25ba3626a0022c55153cc65a1b16cb
HANDZCZ <handz@email.cz>
2024-10-17 19:19:05 +0200

    Add utility function for getting actix::Error from error

--- src/util.rs
+++ src/util.rs
@@ -1,14 +1,21 @@
 use std::fmt::Debug;
 
-use actix_web::{http::StatusCode, HttpResponse, ResponseError};
+use actix_web::{http::StatusCode, Error, HttpResponse};
 use serde::Serialize;
 
 use crate::{empty_error::EmptyError, json_error::JsonError, IS_DEBUG_ON};
 
-pub fn response_from_error(error: impl Serialize + Debug, status_code: StatusCode) -> HttpResponse {
+pub fn response_from_error(
+    error: impl Serialize + Debug + 'static,
+    status_code: StatusCode,
+) -> HttpResponse {
+    get_actix_error(error, status_code).error_response()
+}
+
+pub fn get_actix_error(error: impl Serialize + Debug + 'static, status_code: StatusCode) -> Error {
     if unsafe { IS_DEBUG_ON } {
-        JsonError::new(error, status_code).error_response()
+        JsonError::new(error, status_code).into()
     } else {
-        EmptyError::new(status_code).error_response()
+        EmptyError::new(status_code).into()
     }
 }
