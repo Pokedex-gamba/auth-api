f8d3e026c4c86102f7f8bac8808a589165ed940a
HANDZCZ <handz@email.cz>
2024-10-18 12:45:34 +0200

    Add util method for getting config error handler

--- src/util.rs
+++ src/util.rs
@@ -1,9 +1,13 @@
 use std::fmt::Debug;
 
-use actix_web::{http::StatusCode, Error, HttpResponse};
+use actix_web::{http::StatusCode, Error, HttpRequest, HttpResponse};
 use serde::Serialize;
 
-use crate::{empty_error::EmptyError, json_error::JsonError, IS_DEBUG_ON};
+use crate::{
+    empty_error::{self, EmptyError},
+    json_error::{self, JsonError},
+    IS_DEBUG_ON,
+};
 
 pub fn response_from_error(
     error: impl Serialize + Debug + 'static,
@@ -17,5 +21,16 @@ pub fn get_actix_error(error: impl Serialize + Debug + 'static, status_code: Sta
         JsonError::new(error, status_code).into()
     } else {
         EmptyError::new(status_code).into()
+    }
+}
+
+pub fn get_config_error_handler<E>() -> impl Fn(E, &HttpRequest) -> Error
+where
+    E: actix_web::ResponseError + 'static,
+{
+    if unsafe { IS_DEBUG_ON } {
+        json_error::config_json_error_handler
+    } else {
+        empty_error::config_empty_error_handler
     }
 }
